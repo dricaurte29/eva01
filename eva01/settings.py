c25f5c28cff197d330d3aca4da8b020fc71ba74c
David Ricaurte <dricaurte@unal.edu.co>
2021-01-27 21:30:00 -0500

    ignore

--- eva01/settings.py
+++ eva01/settings.py
@@ -12,7 +12,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 
 from pathlib import Path
 import os
-# esto no debio salir
+# esto no debio salir, ahora si porfa
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
