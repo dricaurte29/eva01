feb8af464112f46c823a3fd89c6929e46685bf89
David Andrés Ricaurte <60167205+dricaurte29@users.noreply.github.com>
2021-02-12 21:59:37 -0500

    fix settings

--- eva01/settings.py
+++ eva01/settings.py
@@ -106,11 +106,14 @@ WSGI_APPLICATION = 'eva01.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'developer',
+        'USER': 'duk',
+        'PASSWORD': 'cundinamarca89',
+        'PORT': 5432,
+        'HOST': 'localhost',
     }
 }
-
 
 
 # Password validation
