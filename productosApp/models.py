cefba3ef4faecd790ff4339422b3f9620b690e82
David Andrés Ricaurte <60167205+dricaurte29@users.noreply.github.com>
2021-02-26 14:07:53 -0500

    Update models.py

--- productosApp/models.py
+++ productosApp/models.py
@@ -29,6 +29,11 @@ class tienda(models.Model):
         verbose_name_plural='tiendas'
     def __str__(self):
         return self.nombre
+    def get_absolute_url(self):
+        
+        return reverse("local", args=[str(self.url)])
+    
+    
 class categoria(models.Model):
     nombre=models.CharField(max_length=50)
 
